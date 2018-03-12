c3b9059b9e294b262121f41c0b533cecfd2667c8
Andy Sipple <Andy>
2018-03-11 22:23:48 -0500

    Add bluma and better filtering

--- src/components/space_x.vue
+++ src/components/space_x.vue
@@ -1,38 +1,44 @@
 <template>
-  <div class="container">
-    <div class="filters">
-      <h3>Filters</h3>
-      <div class="field-group">
-        <input type="checkbox" v-model="show_failed_rockets">
-        <label>Show rockets that failed launch.</label>
-      </div>
-    </div>
-    <transition-group name="card" tag="div" class="cards-wrapper">>
-      <div v-for="rocket in filterRockets.slice().reverse()" v-bind:key="rocket.flight_number" class="card">
-        <div class="card__left">
-          <div class="card__image">
-            <img :src="rocket.links.mission_patch" />
-          </div>
+  <div class="container is-fluid">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container is-fluid">
+        <h1 class="title">SpaceX API Data</h1>
+        <div class="buttons">
+          <button class="button button-filter" @click="filterRockets($event, 'launch_success=true')">Successful Launches {{successful_rockets}}</button>
+          <button class="button button-filter" @click="filterRockets($event, 'launch_success=false')">Failed Launches {{ failed_rockets}}</button>
+          <button class="button button-filter" @click="filterRockets($event, 'land_success=true')">Landing Successful {{ land_success }}</button>
+          <button class="button button-filter" @click="filterRockets($event, 'land_success=false')">Landing Failed {{ land_failed }}</button>
+          <button class="button button-filter" @click="filterRockets($event, 'reused=true')">Cores Reused {{ cores_reused }}</button>
+          <button class="button" @click="filterRockets($event, 'clear')">Clear</button>
         </div>
-        <div class="card__right">
-          <ul>
-          <li>Flight Number: {{ rocket.flight_number }}</li>
-          <li>Launch Date: {{ launch_date(rocket.launch_date_utc) }}</li>
-          <li v-if="rocket.launch_success">
-          Rocket Launch Successful.
-          </li>
-          <li v-else-if="!rocket.details">
-          Launch Pending
-          </li>
-          <li v-else >
-          Rocket Launch Failed.
-          </li>
-          </ul>
-          <p>{{ rocket.details }}</p>
-          <a v-if="rocket.links.video_link" v-bind:href="rocket.links.video_link">Video Link</a>
-          <router-link :to="{ name: 'rocket', params: { id: rocket.flight_number }}">
-          Learn More
-          </router-link>
+        </div>
+      </div>
+    </section>
+    <transition-group name="card" tag="div" class="columns is-multiline is-variable is-3">
+      <div v-for="rocket in rockets.slice().reverse()" v-bind:key="rocket.flight_number" class="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-128x128">
+              <img :src="rocket.links.mission_patch" />
+            </figure>
+          </div>
+          <div class="card-content">
+            <time>Launch Date: {{ launch_date(rocket.launch_date_utc) }}</time>
+            <p>Flight Number: {{ rocket.flight_number }}</p>
+            <p v-if="rocket.launch_success">Rocket Launch Successful.</p>
+            <p v-else> Rocket Launch Failed.</p>
+            <div v-for="(core, index) in rocket.rocket.first_stage.cores">
+              <p v-if="core.land_success">Core {{ index + 1 }} landing successful.</p>
+              <p v-else>Core {{index + 1 }} landing failed.</p>
+            </div>
+          </div>
+          <footer class="card-footer">
+              <a v-bind:href="rocket.links.video_link" class="card-footer-item button is-primary">Watch Launch</a>
+              <router-link :to="{ name: 'rocket', params: { id: rocket.flight_number }}" class="card-footer-item button is-primary">
+              Learn More
+              </router-link>
+          </footer>
         </div>
       </div>
     </transition-group>
@@ -47,62 +53,115 @@
     data () {
       return {
           rockets: [],
-          show_failed_rockets: false
+          successful_rockets: 0,
+          failed_rockets: 0,
+          cores_reused: 0,
+          land_success: 0,
+          land_failed: 0,
+          filter_data: [],
       }
     },
     created () {
       axios.get('https://api.spacexdata.com/v2/launches').then(response => {
           this.rockets = response.data;
+          for (let rocket of response.data) {
+            if(rocket.launch_success) {
+              this.successful_rockets++;
+            }
+            else {
+              this.failed_rockets++;
+            }
+            if(rocket.reuse.core) {
+              this.cores_reused++;
+            }
+            if(rocket.rocket.first_stage.cores) {
+              for (let core of rocket.rocket.first_stage.cores) {
+                if(core.land_success) {
+                  this.land_success++;
+                }
+                else {
+                  this.land_failed++;
+                }
+              }
+            }
+          }
       });
     },
     methods: {
       launch_date: function (date) {
-        return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+        return moment(date).format('MMM Do YYYY');
+      },
+      filterRockets: function($event, options) {
+        $event.target.classList.toggle('is-active');
+        let button_filters = document.getElementsByClassName('button-filter');
+        if($event.target.classList.contains('is-active')) {
+          if(!this.filter_data.includes(options)) {
+              this.filter_data.push(options);
+          }
+        }
+        else {
+          this.filter_data = removeArrayItem(this.filter_data, options);
+        }
+        if(options === 'clear') {
+          this.filter_data = [];
+          for (let button of button_filters) {
+            if(button.classList.contains('is-active')) {
+              button.classList.remove('is-active');
+            }
+          }
+        }
+        let query = Object.keys(this.filter_data).map(k => this.filter_data[k]).join('&');
+        axios.get(`https://api.spacexdata.com/v2/launches?${query}`).then(response => {
+            this.rockets = response.data;
+            this.successful_rockets = 0;
+            this.failed_rockets = 0;
+            this.cores_reused = 0;
+            this.land_success = 0;
+            this.land_failed = 0;
+            for (let rocket of response.data) {
+              if(rocket.launch_success) {
+                this.successful_rockets++;
+              }
+              else {
+                this.failed_rockets++;
+              }
+              if(rocket.reuse.core) {
+                this.cores_reused++;
+              }
+              if(rocket.rocket.first_stage.cores) {
+                for (let core of rocket.rocket.first_stage.cores) {
+                  if(core.land_success) {
+                    this.land_success++;
+                  }
+                  else {
+                    this.land_failed++;
+                  }
+                }
+              }
+            }
+        });
       }
     },
     computed: {
-      filterRockets: function() {
-        let showRocketFailed = this.show_failed_rockets;
-        return this.rockets.filter((rocket) => {
-          if(showRocketFailed) {
-            if(rocket.launch_success == false) {
-              return rocket;
-            }
-          }
-          else {
-            return rocket;
-          }
-        });
-      }
     }
+  }
+  const removeArrayItem = (arr, itemToRemove) => {
+    return arr.filter(item => item !== itemToRemove)
   }
 </script>
 
 <style lang='scss' scoped>
-  .container {
-    // display: grid;
-    // grid-columns: auto minmax(min-content, 1fr);
-    padding: 20px;
-  }
-  .filters {
-    grid-column: 1;
-    min-width: 250px;
-  }
-  .cards-wrapper {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 40px;
-    grid-column: 2;
-  }
   .card {
-    padding: 20px;
-    min-height: 320px;
-    box-shadow: rgba(0, 0, 0, 0.19) 0 0 8px 0;
-    &__image {
-      max-width: 250px;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    &-image {
+      padding: 40px 0;
+      .image {
+        margin: auto;
+      }
       img {
-        width: 100%;
-        object-fit: contain;
+        max-height: 100%;
       }
     }
     &-move {
@@ -124,6 +183,16 @@
     }
     &-enter {
       transform: scale(0.4);
+    }
+  }
+  .card-footer {
+    margin-top: auto;
+    &-item {
+      border-radius: 0;
+      &:not(:last-child) {
+        border: none;
+        margin-right: 5px;
+      }
     }
   }
 </style>
